b4fd8feef934dc18750cb031af4887e33b5b344f
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2024-01-08 00:17:08 -0500

    cors...

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,7 +1,10 @@
+import LoginForm from "~/components/login_form";
+
 export default async function Login() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       This is the login page
+      <LoginForm />
     </div>
   );
 }
